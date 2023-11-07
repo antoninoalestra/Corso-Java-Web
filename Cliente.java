4967da1cb908666243a74833cb25be1ad45f4627
antoninoalestra <ninoalestra@gmail.com>
2023-11-07 16:15:16 +0100

    chunk 1: Giocattolo e Cliente

---
+++ Cliente.java
@@ -0,0 +1,38 @@
+public class Cliente {
+    private static int idProgressivo = 1;
+    private int id;
+    private String nome;
+    private String email;
+
+    public Cliente(int id, String nome, String email){
+        this.id = idProgressivo;
+        this.nome = nome;
+        this.email = email;
+        idProgressivo++;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    @Override
+    public String toString(){
+        return "Cliente numero " + this.id + ":\nNome: " + this.nome + "\nEmail: " + this.email;
+    }
+}
